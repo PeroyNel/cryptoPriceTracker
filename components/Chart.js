5a42cfd43c35d10cb0e0fc5a042159aaea39b4c3
Peroy Nel <peroynel@yahoo.com>
2022-03-10 13:18:32 +0200

    Chart Component Styling Added
    Chart Component Styling Added

--- components/Chart.js
+++ components/Chart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const Chart = ({ currentPrice, logoUrl, name, symbol, priceChange, sparkline }) => {
+  const priceChangeColor = priceChange > 0 ? '#34C759' : '#FF3830';
     return (
     <View style={styles.chartWrapper}>
       <View style={styles.titleWrapper}>
@@ -14,7 +15,7 @@ const Chart = ({ currentPrice, logoUrl, name, symbol, priceChange, sparkline }) 
         </View>
         <View style={styles.lowerTitles}>
           <Text style={styles.boldTitle}>${currentPrice.toLocaleString('en-US', {currency: 'USD'})}</Text>
-          <Text style={styles.title}>{priceChange.toFixed(2)}%</Text>
+          <Text style={[styles.title, {color: priceChangeColor}]}>{priceChange.toFixed(2)}%</Text>
         </View>
       </View>
     </View>
@@ -22,7 +23,42 @@ const Chart = ({ currentPrice, logoUrl, name, symbol, priceChange, sparkline }) 
 }
 
 const styles = StyleSheet.create({
-
+  chartWrapper: {
+    margin: 16,
+  },
+  upperTitles: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  image: {
+    width: 24,
+    height: 24,
+    marginRight: 4,
+  },
+  subtitle: {
+    fontSize: 14,
+    color: '#A9ABB1',
+  },
+  upperLeftTitle: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  lowerTitles: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginLeft: 0,
+    
+  },
+  boldTitle: {
+    fontSize: 24,
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  title: {
+    fontSize: 18,
+  }
 });
 
 export default Chart;
